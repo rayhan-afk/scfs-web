2c829385acff3dd821bb6b427787c259c9f3e33d
Rayhan Abduhuda <rayrayhan920@gmail.com>
2026-05-18 14:03:02 +0700

    Update MerchantProduct.php

--- app/Models/MerchantProduct.php
+++ app/Models/MerchantProduct.php
@@ -6,15 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class MerchantProduct extends Model
 {
-   protected $fillable = [
+    // Tambahkan 'stok' di sini!
+    protected $fillable = [
         'merchant_id', 'nama_produk', 'foto_produk', 
-        'kategori', 'harga_pokok', 'harga_jual', 'is_tersedia'
+        'kategori', 'harga_pokok', 'harga_jual', 'is_tersedia', 'stok'
     ];
 
     public function merchant() {
         return $this->belongsTo(User::class, 'merchant_id');
     }
+    
     public function priceHistories() {
-    return $this->hasMany(ProductPriceHistory::class)->latest();
-}
+        return $this->hasMany(ProductPriceHistory::class)->latest();
+    }
 }
